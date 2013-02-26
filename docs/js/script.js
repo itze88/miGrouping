ce9f0ae6aa36c8d3aa23c2b94e865462a6cc4a63
Marc Itzenthaler <itzenthaler@teleschau.de>
2013-02-26 15:51:23 +0100

    Example added

---
+++ docs/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.test1').miGrouping({
+        
+    });
+    $('.test2').miGrouping({
+        changeAll: true
+    });
+    $('.test3').miGrouping({
+        ajaxContent: true
+    });
+});
